36e1a236c48e18ea8ad9e214d3b54d9b5a375e60
GabrielVanco <gabrielvanco111223@gmail.com>
2025-06-17 21:39:43 -0300

    Adicionando funcionalidade do menu-hamburguer em telas pequenas

--- src/js/home.js
+++ src/js/home.js
@@ -1,1 +1,43 @@
-// Links dinamicos
+// Rolamento dinamico
+
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+// Adiciona o evento de clique aos links de navegação
+const navLinks = document.querySelectorAll(".nav-link");
+navLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const sectionId = link.getAttribute("href").substring(1);
+    scrollToSection(sectionId);
+  });
+});
+
+// Adiciona evento de rolagem no botão cadastrar necessidades
+const cadastroLink = document.querySelector(".cadastro-link");
+cadastroLink.addEventListener("click", () => {
+  scrollToSection("cadastro");
+});
+
+// Adiciona o evento de clique ao botão de rolagem para o topo
+const scrollToTopButton = document.querySelector(".scroll-to-top");
+scrollToTopButton.addEventListener("click", () => {
+  scrollToSection("home");
+});
+
+// Menu hamburguer mobile
+
+function toggleMobileMenu() {
+  const navMenu = document.getElementById("navMenu");
+  const toggle = document.querySelector(".mobile-toggle");
+
+  navMenu.classList.toggle("active");
+  toggle.classList.toggle("open"); /**
+
+ * Fecha o menu mobile
+ */
+}
